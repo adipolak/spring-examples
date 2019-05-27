4b52a55965c709d8f8e9d186f8edfa3f34f1b828
Yaohai Zheng <yaozheng@microsoft.com>
2019-05-27 10:15:29 +0800

    Add initial solution

---
+++ initial/src/main/java/org/springframework/samples/petclinic/pet/MyPetRepository.java
@@ -0,0 +1,24 @@
+package org.springframework.samples.petclinic.pet;
+
+import java.util.Collection;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.repository.Repository;
+import org.springframework.samples.petclinic.owner.Pet;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * PetRepository
+ * 
+ * @param <Pet>
+ */
+public interface MyPetRepository extends Repository<Pet, Integer> {
+
+        /**
+     * Retrieve all <code>Vet</code>s from the data store.
+     *
+     * @return a <code>Collection</code> of <code>Vet</code>s
+     */
+    @Transactional(readOnly = true)
+    Collection<Pet> findAll() throws DataAccessException;
+}
